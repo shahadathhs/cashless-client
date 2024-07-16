0879e8b47e01b8d515d1c18c6529b4b77b7fe8a0
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-07-16 17:50:49 +0600

    fixed: login direction error

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,8 @@ import { Helmet } from "react-helmet-async";
 import { useAuth } from "../provider/AuthProvider";
 
 export default function Dashboard() {
-  const {user} = useAuth()
-  console.log(user)
+  const { user } = useAuth();
+  console.log(user);
 
   return (
     <div>
@@ -11,24 +11,21 @@ export default function Dashboard() {
         <title>Dashboard | Cashless</title>
       </Helmet>
       <div className="p-4 space-y-4">
-        <h1 className="text-2xl text-center font-bold">Welcome to your dashboard!</h1>
+        <h1 className="text-2xl text-center font-bold">
+          Welcome to your dashboard!
+        </h1>
         <div>
-        <div className="flex justify-center">
-      {user ? (
-        <div className="card-body card-bordered shadow-md max-w-md">
-          <p className="card-title">Welcome, {user.name}!</p>
-          <p>Email: {user.email}</p>
-          <p>Role: {user.role}</p>
-          <p>Phone: {user.phone}</p>
-          <p>Balance: {user.balance}</p>
+          <div className="flex justify-center">
+            <div className="card-body card-bordered shadow-md max-w-md">
+              <p className="card-title">Welcome, {user?.name}!</p>
+              <p>Email: {user?.email}</p>
+              <p>Role: {user?.role}</p>
+              <p>Phone: {user?.phone}</p>
+              <p>Balance: {user?.balance}</p>
+            </div>
+          </div>
         </div>
-      ) : (
-        <p className="text-center font-bold">Loading user data...</p>
-      )}
       </div>
     </div>
-        
-      </div>
-    </div>
-  )
+  );
 }
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,34 @@
-import { Navigate } from "react-router-dom";
+import { Navigate} from "react-router-dom";
 import PropTypes from 'prop-types';
+import { useAuth } from "../provider/AuthProvider";
 
-export default function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
-}
+const PrivateRoutes = ({children}) => {
+  const {user, token} = useAuth();
 
-PrivateRoute.propTypes = {
+  if (!token) {
+    <Navigate to="/login"></Navigate>
+  }
+
+  if(!user){
+    window.location.reload();
+    return (
+    <div className='w-full h-[300px] flex justify-center items-center text-3xl'>
+      <span className="loading loading-spinner text-primary"></span>
+      <span className="loading loading-spinner text-secondary"></span>
+      <span className="loading loading-spinner text-accent"></span>
+      <span className="loading loading-spinner text-neutral"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-error"></span>
+    </div>)
+  }
+
+  return children;
+};
+
+export default PrivateRoutes;
+
+PrivateRoutes.propTypes = {
   children: PropTypes.node,
 }
